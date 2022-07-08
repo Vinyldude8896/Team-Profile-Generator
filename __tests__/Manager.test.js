d5b17826c47cafd23fd036b6c79fa94e4a44cca7
Kevin Reid <kevinnivek@me.com>
2022-07-08 12:49:48 -0400

    added manager inputs and verification

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -12,3 +12,22 @@ test ('Creates a new Manager object', () => {
     expect(manager.role).toEqual('Manager');
 })
 
+test ("Get Manager's name", () => {
+    const manager = new Manager('Kevin Reid', '001555888', "kevinnivek@me.com", '256', 'Manager')
+    expect(manager.getName()).toEqual(expect.any(String));
+})
+
+test ("Get Manager's employee ID", () => {
+    const manager = new Manager('Kevin Reid', '001555888', "kevinnivek@me.com", '256', 'Manager')
+    expect(manager.getId()).toEqual(expect.any(String));
+})
+
+test ("Get Manager's email address", () => {
+    const manager = new Manager('Kevin Reid', '001555888', "kevinnivek@me.com", '256', 'Manager')
+    expect(manager.getEmail()).toEqual(expect.any(String));
+})
+
+test ("Get Manager's role",  () =>{
+    const manager = new Manager('Jacob Jones', '0098456', 'jacobjones@yahooo.com', '256', 'Manager')
+    expect(manager.getRole()).toEqual('Manager');
+})
